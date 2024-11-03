5694702c5ac5383538242275e137f79333052e53
kennyboy106 <tang.kenneth720@gmail.com>
2024-11-02 17:47:20 -0700

    Finish example.py

--- tests/example.py
+++ tests/example.py
@@ -1,6 +1,8 @@
 from fidelity import fidelity
 
 browser = fidelity.FidelityAutomation(headless=False, save_state=False)
+
+# Login
 step_1, step_2 = browser.login(username="USER", password="PASS", save_device=True)
 if step_1 and step_2:
     print("Logged in")
@@ -13,11 +15,18 @@ else:
     print("Browser not logged in")
     exit(1)
 
+# Get accounts
 account_info = browser.getAccountInfo()
 accounts = account_info.keys()
+
 # Test the transaction
 success, errormsg = browser.transaction("INTC", 1, 'buy', accounts[0], True)
 if success:
     print("Successfully tested transaction")
 else:
     print(errormsg)
+
+# Print withdrawal balance from each account
+acc_dict = browser.get_list_of_accounts(set_flag=True, get_withdrawal_bal=True)
+for account in acc_dict:
+    print(f"{acc_dict[account]['nickname']}:  {account}: {acc_dict[account]['withdrawal_balance']}")
